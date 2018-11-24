402dd39a4880464381c6ddd3813b5b812188d4ee
smm-tm <smm-tm@Hotmail.com>
2018-11-24 16:07:14 +0200

    ok

---
+++ App22/ContentFragment.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V4.App;
+using Android.Support.V4.View;
+using Android.Views;
+using Android.Widget;
+
+namespace App22
+{
+    class ContentFragment : Fragment
+    {
+        private int position;
+        public static ContentFragment NewInstance(int postion)
+        {
+            var f = new ContentFragment();
+            var b = new Bundle();
+            b.PutInt("position", postion);
+
+            f.Arguments = b;
+            return f;
+        }
+
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            position = Arguments.GetInt("position");
+        }
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            var root = inflater.Inflate(Resource.Layout.Fragment, container, false);
+            var text = root.FindViewById<TextView>(Resource.Id.textView);
+            if (position == 0)
+            {
+                text.Text = "P Android";
+
+            }else if(position == 1)
+            {
+                text.Text = "P iOS";
+
+            }
+            else if (position == 2)
+            {
+                text.Text = "P UWP";
+                
+            }
+            ViewCompat.SetElevation(root, 50);
+            return root;
+        }
+    }
+}
--- App22/MainActivity.cs
+++ App22/MainActivity.cs
@@ -2,41 +2,75 @@
 using Android.OS;
 using Android.Runtime;
 using Android.Support.Design.Widget;
+using Android.Support.V4.App;
+using Android.Support.V4.View;
 using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
+using com.refractored;
+using Java.Lang;
 
 namespace App22
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity, BottomNavigationView.IOnNavigationItemSelectedListener
+    [Activity(Label = "@string/app_name", Theme = "@style/Theme.AppCompat.Light.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity 
     {
         TextView textMessage;
-
+        MyAdapter adapter;
+        PagerSlidingTabStrip pageSlidingTabStrip;
+        ViewPager viewPager;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+
+
+           // ActionBar.NavigationMode = ActionBarNavigationMode.Tabs;
             SetContentView(Resource.Layout.activity_main);
 
-            textMessage = FindViewById<TextView>(Resource.Id.message);
-            BottomNavigationView navigation = FindViewById<BottomNavigationView>(Resource.Id.navigation);
-            navigation.SetOnNavigationItemSelectedListener(this);
+            adapter = new MyAdapter(SupportFragmentManager);
+            viewPager = FindViewById<ViewPager>(Resource.Id.pager);
+            pageSlidingTabStrip = FindViewById<PagerSlidingTabStrip>(Resource.Id.tabs);
+
+            viewPager.Adapter = adapter;
+            pageSlidingTabStrip.SetViewPager(viewPager);
+            pageSlidingTabStrip.SetBackgroundColor(Android.Graphics.Color.Argb(255,0,149,164));
         }
-        public bool OnNavigationItemSelected(IMenuItem item)
+        
+    }
+
+    public class MyAdapter : FragmentPagerAdapter
+    {
+        int tabCount = 2;
+        public MyAdapter(Android.Support.V4.App.FragmentManager _fragmentManager): base (_fragmentManager)
         {
-            switch (item.ItemId)
+            
+        }
+        public override int Count { get { return tabCount; } }
+
+        public override Android.Support.V4.App.Fragment GetItem(int position)
+        {
+          return  ContentFragment.NewInstance(position);
+        }
+
+
+        public override ICharSequence GetPageTitleFormatted(int position)
+        {
+            ICharSequence CS = new Java.Lang.String("");
+            if (position == 0)
             {
-                case Resource.Id.navigation_home:
-                    textMessage.SetText(Resource.String.title_home);
-                    return true;
-                case Resource.Id.navigation_dashboard:
-                    textMessage.SetText(Resource.String.title_dashboard);
-                    return true;
-                case Resource.Id.navigation_notifications:
-                    textMessage.SetText(Resource.String.title_notifications);
-                    return true;
+                CS = new Java.Lang.String("Android");
             }
-            return false;
+            else if (position == 1)
+            {
+                CS = new Java.Lang.String("iOS");
+
+            }
+            else if (position == 2)
+            {
+                CS = new Java.Lang.String("UWP");
+
+            }
+            return CS;
         }
     }
 }
